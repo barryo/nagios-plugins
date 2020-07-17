6da35064bd679531bf4a07ae759ef85311c5c534
Barry O'Donovan <barry@islandbridgenetworks.ie>
2020-07-17 09:54:52 +0100

    Per @nickhilliard

--- check_arista_chassis_api.php
+++ check_arista_chassis_api.php
@@ -226,7 +226,7 @@ exit( $status );
 function checkCPU( $l1, $l2, $l3 ) {
     global $cmdargs, $criticals, $warnings, $unknowns, $normals;
 
-    if( $l1 > 1.5 || $l2 > 1.5 || $l3 > 1.5 ) {
+    if( $l1 > 2 || $l2 > 2 || $l3 > 2 ) {
         setStatus( STATUS_WARNING );
         $criticals .= "System load is high: {$l1} {$l2} {$l3}. ";
     } else {
